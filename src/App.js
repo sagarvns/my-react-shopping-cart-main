57e43bb4cc6ed90c79edeb968b24b9540e86e7f7
sagarvns <90026062+sagarvns@users.noreply.github.com>
2022-08-24 13:28:12 +0530

    update
    And New File Sum Of Two element

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import './App.css'
 import Header from './components/Header';
 import Test from './components/Test'
 
+import Add from './components/add'
+
 
 
  function App() {
   return (<>
-  <Header />
+  {/* <Header />
   <Test/>
 <Test/>
 <Test/>
 <Test/>
 <Test/>
-<Test/>
+<Test/> */}
+<Add/>
 
     </>
   )
---
+++ src/components/add.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+const VsjForm = () => {
+    const [amt1, setAmt1] = useState(0);
+    const [amt2, setAmt2] = useState(0);
+    const amountChangeHandler1 = (event) => {
+
+        console.log("Amount Changed");
+        console.log(event.target.value);
+        setAmt1(Number(event.target.value));
+    };
+    const amountChangeHandler2 = (event) => {
+
+        console.log("Amount Changed");
+        console.log(event.target.value);
+        setAmt2(Number(event.target.value));
+    };                                      
+
+
+
+    return <form>
+        <label>Amount</label>
+        <input type="text" onChange={amountChangeHandler1} />
+        <input type="text" onChange={amountChangeHandler2} />
+        <h1>{amt1}</h1>
+        <h1>{amt2}</h1>
+        <h1>{Number(amt1) + Number(amt2)}</h1>
+    </form>
+}
+export default VsjForm;
